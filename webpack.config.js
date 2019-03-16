5d754e76b0f0a3977209038137efe68ec8ffcb28
Gaspar Teixeira <gaspar.teixeira@gmail.com>
2019-03-15 23:31:09 -0400

    correcting icons paths

--- webpack.config.js
+++ webpack.config.js
@@ -98,12 +98,12 @@ module.exports = (env, options) => {
         description: "Manifest for Gaspar Teixeira",
         icons: [
           {
-            src: "/images/android-chrome-192x192.png",
+            src: "./public/images/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/images/android-chrome-512x512.png",
+            src: "./public/images/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
